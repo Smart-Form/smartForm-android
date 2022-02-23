fd977e7842691322fc99dbccb840900f96151574
Cyrus Lau <cyruslau.work@gmail.com>
2022-02-23 21:22:45 +0800

    origin

---
+++ app/src/main/java/com/example/smartformandroid/id_card.java
@@ -0,0 +1,14 @@
+package com.example.smartformandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class id_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.id_card);
+    }
+}
