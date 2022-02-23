2141951c9aebfb4e4fcad726fb5426364109a1f4
Cyrus Lau <cyruslau.work@gmail.com>
2022-02-23 22:31:58 +0800

    origin

---
+++ app/src/main/java/com/example/smartformandroid/data_allow.java
@@ -0,0 +1,14 @@
+package com.example.smartformandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class data_allow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_allow);
+    }
+}
